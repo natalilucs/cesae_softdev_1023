fe34323a5d8a61d3acc9d36241bc411cdc432a73
Natali Lucas <natali_l@hotmail.com>
2023-10-19 11:56:34 +0100

    Ex01
    resolvido

--- FichasJava/src/FichaPratica01/Ex03.java
+++ FichasJava/src/FichaPratica01/Ex03.java
@@ -4,21 +4,6 @@ import java.util.Scanner;
 
 public class Ex03 {
 
-    public static void main(String[] args) {
-
-        //Instanciar o scanner
-
-        Scanner input = new Scanner(System.in);
-
-        // Ler numero 1
-        System.out.println("Insira um numero");
-        int number1 = input.nextInt();
-        System.out.println("Insira outro numero");
-        int number2 = input.nextInt();
-        int result = number1 + number2;
-
-        System.out.println("O resultado da soma dos dois numeros, é: " + result);
-
 
 
     }
