e669647612ff80e226a1222b6e197b10585074a8
Natali Lucas <1222649@isep.ipp.pt>
2023-10-20 12:52:36 +0100

    ex08
    resolvidos

--- FichasJava/src/FichaPratica01/Ex08.java
+++ FichasJava/src/FichaPratica01/Ex08.java
@@ -5,6 +5,30 @@ import java.util.Scanner;
 public class Ex08 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int minutos, segundos, music1, music2,music3,music4, music5;
+        int minutosTotais, segundosTotais, music1, music2,music3,music4, music5;
+
+        System.out.print("Insira os minutos da musica 1: ");
+        minutosTotais = input.nextInt();
+        System.out.print("Insira os segundos da musica 1: ");
+        segundosTotais = input.nextInt();
+        System.out.print("Insira os minutos da musica 2: ");
+        minutosTotais = minutosTotais + input.nextInt();
+        System.out.print("Insira os segundos da musica 2: ");
+        segundosTotais = segundosTotais + input.nextInt();
+        System.out.print("Insira os minutos da musica 3: ");
+        minutosTotais = minutosTotais + input.nextInt();
+        System.out.print("Insira os segundos da musica 3: ");
+        segundosTotais = segundosTotais + input.nextInt();
+        System.out.print("Insira os minutos da musica 4: ");
+        minutosTotais = minutosTotais + input.nextInt();
+        System.out.print("Insira os segundos da musica : ");
+        segundosTotais = segundosTotais + input.nextInt();
+        System.out.print("Insira os minutos da musica 5: ");
+        minutosTotais = minutosTotais + input.nextInt();
+        System.out.print("Insira os segundos da musica 5: ");
+        segundosTotais = segundosTotais + input.nextInt();
+
+        System.out.print(minutosTotais +"min" +segundosTotais + "s");
+
     }
 }
