ea984d9bad795eba4a86a115d63d7c98096797ad
Natali Lucas <1222649@isep.ipp.pt>
2023-10-20 11:43:58 +0100

    ex08

---
+++ FichasJava/src/FichaPratica01/Ex08.java
@@ -0,0 +1,10 @@
+package FichaPratica01;
+
+import java.util.Scanner;
+
+public class Ex08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int minutos, segundos, music1, music2,music3,music4, music5;
+    }
+}
