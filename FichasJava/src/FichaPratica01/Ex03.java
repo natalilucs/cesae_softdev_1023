a013d3ac1f459e7202fbd11197ad9fd66d5ae202
Natali Lucas <1222649@isep.ipp.pt>
2023-10-20 10:05:57 +0100

    ex03 e ex04
    Resolução de dois exercícios

--- FichasJava/src/FichaPratica01/Ex03.java
+++ FichasJava/src/FichaPratica01/Ex03.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class Ex03 {
 
-
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double pi = 3.14;
+        System.out.println("Informe o raio: ");
+        double raio = input.nextDouble();
+        double area = raio * raio * pi;
+        double perimetro = 2.0 * pi * area;
+        System.out.println("o resultado do perimetro é: " + perimetro);
     }
+
+
 }
