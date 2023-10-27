449c5a61bd2515a6a22924f66ca43cd53e6323dd
Natali Lucas <1222649@isep.ipp.pt>
2023-10-27 16:18:26 +0100

    ex11 corrigir

--- FichasJava/src/FichaPratica03/Ex11.java
+++ FichasJava/src/FichaPratica03/Ex11.java
@@ -6,15 +6,30 @@ public class Ex11 {
     public static void main(String[] args) {
         Scanner input =  new Scanner(System.in);
 
-        int numInteiro = 5, contador;
+        int numInteiro = 5, contador = 0;
 
-        while(numInteiro != -1){
+        while(numInteiro > 0){
             System.out.println("Adivinhe o número que pare o programa: ");
             numInteiro = input.nextInt();
-            //contador = numInteiro+1;
-            if(numInteiro > 0 && numInteiro < 25){
-                System.out.println("[00,25]" + numInteiro);
+
+            if(numInteiro > 0 && numInteiro <= 25){
+                contador = contador + 1;
+
+            } else if (numInteiro > 25 && numInteiro <= 50){
+                contador = contador + 1;
+
+            } else if (numInteiro > 50 && numInteiro <= 75){
+                contador = contador + 1;
+
+            } else if(numInteiro > 76 && numInteiro <= 100){
+                contador = contador + 1;
+
             }
+
         }
+        System.out.println("[00,25]: " + contador);
+        System.out.println("[26,50]: " + contador);
+        System.out.println("[51,75]: " + contador);
+        System.out.println("[76,100]: " + contador);
     }
 }
--- FichasJava/src/FichaPratica03/Ex12.java
+++ FichasJava/src/FichaPratica03/Ex12.java
@@ -1,4 +1,13 @@
 package FichaPratica03;
 
+import java.util.Scanner;
+
 public class Ex12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int first, second;
+
+        System.out.println("");
+
+    }
 }
